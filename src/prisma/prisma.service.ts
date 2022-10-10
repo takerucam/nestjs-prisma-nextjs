abf533472cb94df9881aef4ff8b6d5b79765c99e
Takeru Miyake <m18kecam777@gmail.com>
2022-10-10 15:07:19 +0900

    🚑 Typoの修正

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,7 +8,7 @@ export class PrismaService extends PrismaClient {
     super({
       datasources: {
         db: {
-          url: config.get('DATEBASE_URL'),
+          url: config.get('DATABASE_URL'),
         },
       },
     });
